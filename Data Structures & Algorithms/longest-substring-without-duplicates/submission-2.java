16357415cecf20168ca829ac45e6199dfdcbb890
xcwfawqdyz <1067346167@qq.com>
2026-07-09 13:58:25 +0800

    Add: longest-substring-without-duplicates - submission-2

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int result=0;
+        Map<Character,Integer> map=new HashMap<>();
+        int start=0;
+        for(int i=0;i<s.length();i++){
+            char ch= s.charAt(i);
+            if(map.containsKey(ch)){
+                start=Math.max(map.get(ch)+1,start);
+            }
+
+            result=Math.max(result,i-start+1);
+            
+            map.put(ch,i);
+        }
+        return result;
+    }
+}
