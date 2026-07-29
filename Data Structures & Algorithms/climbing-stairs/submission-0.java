0813eaae9f67e2d391a63778530287a746f41eb8
xcwfawqdyz <1067346167@qq.com>
2026-07-29 09:02:48 +0800

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2) return n;
+        int one=1,two=2;
+        for(int i=3;i<=n;i++){
+            int temp=one+two;
+            one=two;
+            two=temp;
+        }
+        return two;
+    }
+}
