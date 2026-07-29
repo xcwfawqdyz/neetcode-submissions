6141fd2dc1034cef22b9cd14597dead1240b2bb2
xcwfawqdyz <1067346167@qq.com>
2026-07-29 10:40:06 +0800

    Add: palindromic-substrings - submission-2

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int n=s.length();
+        int res=0;
+        for(int i=0;i<n;i++){
+            int l=i,r=i;
+            while(l>=0 && r<n && s.charAt(l)==s.charAt(r)){
+                res++;
+                l--;
+                r++;
+            }
+            l=i;
+            r=i+1;
+            while(l>=0 && r<n && s.charAt(l)==s.charAt(r)){
+                res++;
+                l--;
+                r++;
+            }
+        }
+        return res;
+    }
+}
